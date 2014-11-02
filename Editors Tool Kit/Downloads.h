24379914961f13792424252835d6307906e3f1be
fntMentaL <iminactivee@gmail.com>
2014-11-02 20:12:25 +0200

    Fixes, beeps

--- Editors Tool Kit/Downloads.h
+++ Editors Tool Kit/Downloads.h
@@ -15,6 +15,7 @@ void mainDownloads()
 	cout << "Select a file that you wish to download: " << endl;
 	cout << "1. avifrate" << endl;
 	cout << "2. Black ops II console, NEWEST" << endl;
+	cout << "3. Easy h264 / Compression" << endl;
 	cin >> leanswer;
 	switch (leanswer)
 	{
@@ -24,6 +25,10 @@ void mainDownloads()
 	case 2:
 		system("start https://www.mediafire.com/?6b9je519w11e64c");
 		break;
+	case 3:
+		system("start http://www.mediafire.com/download/a41gs0ezw0e8gab/Easy_h264.exe");
+		break;
+
 	default:
 		cout << "You enetered nothing." << endl;
 		break;
--- Editors Tool Kit/Main.cpp
+++ Editors Tool Kit/Main.cpp
@@ -10,15 +10,22 @@ int answer;
 
 using namespace std;
 
+void pBeep()
+{
+	Beep(500, 500);
+
+}
 
 int main()
 {
-	
+	pBeep();
 	cout << "Hello, and welcome to Editors Toolkit" << endl;
 	cout << "In order to start, select an option: " << endl;
 	cout << "1. Aspect ratio calculator. " << endl;
-	cout << "2. Open an url" << endl;
+	cout << "2. Download editing files." << endl;
+
 	cin >> answer;
+	pBeep();
 	switch (answer)
 	{
 	case 1:
